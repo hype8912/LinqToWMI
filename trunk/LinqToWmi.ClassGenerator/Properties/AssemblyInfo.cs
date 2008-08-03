49228629cc97f976e4e9958ddf89481a0057295b
Atrophius_cp <SND\Atrophius_cp>
2008-08-03 15:54:55 +0000

    Fixed AssemblyInfo on all projects to have relevant and real data.

--- trunk/LinqToWmi.ClassGenerator/Properties/AssemblyInfo.cs
+++ trunk/LinqToWmi.ClassGenerator/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("LinqToWmi.ClassGenerator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Open Box Software")]
-[assembly: AssemblyProduct("LinqToWmi.ClassGenerator")]
-[assembly: AssemblyCopyright("Copyright © Open Box Software 2007")]
+[assembly: AssemblyCompany("LinqToWmi Group")]
+[assembly: AssemblyProduct("LinqToWmi")]
+[assembly: AssemblyCopyright("Copyright © LinqToWmi Group 2008")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.3.*")]
+// [assembly: AssemblyFileVersion("1.0.0.0")]
