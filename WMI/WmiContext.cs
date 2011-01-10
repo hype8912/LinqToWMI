44410197a67b720348cafe32bd947eebf424bb27
Josh <538536+hype8912@users.noreply.github.com>
2011-01-10 13:57:00 +0000

    Merge branch for feature addition.

--- WMI/WmiContext.cs
+++ WMI/WmiContext.cs
@@ -1,59 +1,105 @@
-using System;
-using System.Collections.Generic;
-using System.Collections;
-using System.Text;
-using System.Linq.Expressions;
-using System.Runtime.CompilerServices;
-using System.Management;
-using System.IO;
-using System.Reflection;
+//-----------------------------------------------------------------------
+// <copyright file="WmiContext.cs">
+//     This code is published under the Microsoft Public License 
+//     (Ms-PL). A copy of the license should be distributed with the code. 
+//     It can also be found at the project website: 
+//     http://www.CodePlex.com/linq2wmi. This notice, the author's name, 
+//     and all copyright notices must remain intact in all applications, 
+//     documentation, and source files.
+// </copyright>
+// 
+// <author name="Joshua DeLong" date="12/14/2010 9:39:29 AM" />
+//-----------------------------------------------------------------------
 
 namespace LinqToWmi.Core.WMI
 {
+    #region Usings
+
+    using System;
+    using System.Collections;
+    using System.IO;
+    using System.Management;
+
+    #endregion
+
     /// <summary>
-    /// Represents an WMI context, and is responsible for opening, closing and quering to the WMI 
-    /// We are now using the WMICOntext both as an context and as an session (Ie. Context.CreateSession()), this should be 2 seperate
-    /// entities later on but for time sake, i'll just put them togheter
+    /// Represents an WMI context, and is responsible for opening, closing and
+    /// querying to the WMI  We are now using the WMIContext both as a context
+    /// and as a session (IE. Context.CreateSession()), this should be 2
+    /// separate entities later on but for time sake, I'll just put them
+    /// together.
     /// </summary>
     public class WmiContext : IDisposable
     {
-        private ManagementScope _managementScope = null;
+        #region Usings
 
-        private WmiQueryBuilder _builder = null;
-        private string _host = null;
-        private TextWriter _log = null;
+        private ManagementScope _managementScope;
+
+        private WmiQueryBuilder builder;
+        private string host;
+        private TextWriter _log;
+
+        #endregion
+
+        #region Constructors/Deconstructors
 
         /// <summary>
-        /// Creates an new WMIContext with the specified host parameter
+        /// Initializes a new instance of the <see cref="WmiContext"/> class.
         /// </summary>
-        /// <param name="host"></param>
+        public WmiContext() : this(@"\\.")
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WmiContext"/> class.
+        /// </summary>
+        /// <param name="host">The host.</param>
         public WmiContext(string host)
         {
-            _builder = new WmiQueryBuilder(this);
-            _host = host;
+            this.builder = new WmiQueryBuilder(this);
+            this.host = host;
         }
 
-        /// <summary>
-        /// Make sure the connection exists
-        /// </summary>
-        private void EnsureConnectionCreated()
-        {
-            if (_managementScope == null)
-            {
-                Connect();
-            }
-        }
+        #endregion
+
+        #region Properties
 
         public TextWriter Log
         {
-            set
+            set { this._log = value; }
+        }
+
+        /// <summary>
+        /// Defines the scope of the WMI query management object.
+        /// </summary>
+        public ManagementScope ManagementScope
+        {
+            get
             {
-                _log = value;
+                if (this._managementScope == null)
+                {
+                    this.Connect();
+                }
+
+                return this._managementScope;
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Makes sure the connection exists and is created.
+        /// </summary>
+        private void EnsureConnectionCreated()
+        {
+            if (this._managementScope == null)
+            {
+                this.Connect();
             }
         }
 
         /// <summary>
-        /// Creates in-fact an new WMI Query object
+        /// Creates in-fact a new WMI Query object.
         /// </summary>
         public WmiQuery<T> Source<T>()
         {
@@ -61,77 +107,65 @@ namespace LinqToWmi.Core.WMI
         }
 
         /// <summary>
-        /// Clean-up
+        /// Performs application-defined tasks associated with freeing,
+        /// releasing, or resetting unmanaged resources.
         /// </summary>
         public void Dispose()
         {
-            if (_managementScope != null)
+            if (this._managementScope != null)
             {
-                Disconnect();
+                this.Disconnect();
             }
         }
 
         /// <summary>
-        /// Executes an WMI query (which is actually an wrapped expression)
+        /// Executes an WMI query (which is actually an wrapped expression).
         /// </summary>
+        /// <param name="query">The WMI query.</param>
+        /// <returns>A generic <see cref="IEnumerator"/> for the specified query
+        /// context.</returns>
         internal IEnumerator ExecuteWmiQuery(IWmiQuery query)
         {
-            EnsureConnectionCreated();
-            string wmiQueryStatement = _builder.BuildQuery(query);
+            this.EnsureConnectionCreated();
+            var wmiQueryStatement = this.builder.BuildQuery(query);
 
-            WriteLog(wmiQueryStatement);
+            this.WriteLog(wmiQueryStatement);
 
-            ObjectQuery objQuery = new ObjectQuery(wmiQueryStatement);
-            ManagementObjectSearcher wmiSearcher = new ManagementObjectSearcher(_managementScope, objQuery);
+            var objQuery = new ObjectQuery(wmiQueryStatement);
+            var wmiSearcher = new ManagementObjectSearcher(this._managementScope, objQuery);
 
-            //Generate a new generic type
-            Type genericType = typeof(WmiObjectEnumerator<>).MakeGenericType(query.Type);
-            
-            ConstructorInfo genericCollection = genericType.GetConstructor(new Type[] { typeof(ManagementObjectCollection) });
+            // Generate a new generic type.
+            var genericType = typeof(WmiObjectEnumerator<>).MakeGenericType(query.Type);
+
+            var genericCollection = genericType.GetConstructor(new[] { typeof(ManagementObjectCollection) });
 
             return (IEnumerator)genericCollection.Invoke(new object[] { wmiSearcher.Get() });
         }
 
         /// <summary>
-        /// Connect and create an new management scope
+        /// Connect and create an new management scope.
         /// </summary>
         private void Connect()
         {
-            _managementScope = new ManagementScope(_host, new ConnectionOptions());
+            this._managementScope = new ManagementScope(this.host, new ConnectionOptions());
         }
 
         /// <summary>
-        /// The scope of the WMI query
-        /// </summary>
-        public ManagementScope ManagementScope
-        {
-            get
-            {
-                if (_managementScope == null)
-                {
-                    Connect();
-                }
-
-                return _managementScope;
-            }
-        }
-
-        /// <summary>
-        /// Disconnect 
+        /// Disconnect.
         /// </summary>
         private void Disconnect()
         {
         }
 
         /// <summary>
-        /// Helper function to write to a log
+        /// Helper function to write text data to a log.
         /// </summary>
-        /// <param name="txt"></param>
+        /// <param name="txt">The text to be logged.</param>
         private void WriteLog(string txt)
         {
-            if (_log != null)
+            if (this._log != null)
             {
-                _log.WriteLine(String.Format("LOG: {0}", txt));
+                this._log.WriteLine(@"LOG: " + txt);
             }
         }
     }
